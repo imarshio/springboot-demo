1cd843b3a5430fca52b17884036fb6aaf45dd50d
mashuo <mashuo@shenqingtech.com>
2024-02-28 19:00:28 +0800

    add postgresql

--- demo-orm/demo-jdbc-template/src/test/java/com/marshio/demo/jdbc/SampleTest.java
+++ demo-orm/demo-jdbc-template/src/test/java/com/marshio/demo/jdbc/SampleTest.java
@@ -7,6 +7,8 @@ import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -22,6 +24,10 @@ import java.util.*;
 public class SampleTest {
 
     private final Log logger = LogFactory.getLog(SampleTest.class);
+
+    private static final ExpressionParser SPEL_EXPRESSION_PARSER = new SpelExpressionParser();
+
+    // MySQL
 
     /**
      * @Description JDBCTemplate usage about update(sql, parameters)
@@ -62,13 +68,41 @@ public class SampleTest {
     @Test
     public void test03() {
         JdbcTemplate jdbcTemplate = getJdbcTemplate();
-        String sql = "select * from user";
+        String sql = "select group_concat(name) group_concat from user group by sex";
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("name", "marshio");
         parameters.put("age", 18);
-        List<Map<String, Object>> result =  jdbcTemplate.queryForList(sql);
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql);
+        System.out.println(SPEL_EXPRESSION_PARSER.parseExpression("size() > 0 ? get(0).get('group_concat'): ''").getValue(result));;
+    }
+
+    /**
+     * @Description NamedParameterJdbcTemplate usage about queryForList(sql, parameters)
+     */
+    @Test
+    public void test04() {
+        JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
+        String sql = "select * from user where name = :name and age = :age";
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", "marshio");
+        parameters.put("age", 18);
+        List<Map<String, Object>> result = namedParameterJdbcTemplate.queryForList(sql, parameters);
         result.forEach(System.out::println);
-        // revert commit test 01 commit
+    }
+
+    // postgresql
+
+    /**
+     * @Description NamedParameterJdbcTemplate usage about queryForList(sql, parameters)
+     */
+    @Test
+    public void test05() {
+        JdbcTemplate jdbcTemplate = getPostgreSqlJdbcTemplate();
+        String sql = "select * from tmp";
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql);
+        String s = "insert into stock_surge_telegram(global_id, title, content, publish_time, show_tags_name, auto_tags_name, auto_tags, category, tag_name, tag_code, related_stocks, weight, type, area, filter, ai_insight,aspects) values(:global_id, :title, :content, :publish_time_trans, :show_tags_name, :auto_tags_name, :auto_tags, :category, :tagName, :tagCode, :Activity_0sktqxy, :weight, :type, :area, :filter, :Activity_0cvlg43, :Activity_0k3a4se)";
+        result.forEach(System.out::println);
     }
 
     private static JdbcTemplate getJdbcTemplate() {
@@ -78,6 +112,23 @@ public class SampleTest {
         hikariConfig.setUsername("pyspider_user");
         hikariConfig.setPassword("ND0328qSywfre");
         hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setConnectionTestQuery("SELECT 1");
+        hikariConfig.setMaximumPoolSize(10);
+        hikariConfig.setMinimumIdle(2);
+        hikariConfig.setConnectionTimeout(30000);
+        hikariConfig.setIdleTimeout(600000);
+        hikariConfig.setMaxLifetime(1800000);
+        DataSource dataSource = new HikariDataSource(hikariConfig);
+        return new JdbcTemplate(dataSource);
+    }
+
+    private static JdbcTemplate getPostgreSqlJdbcTemplate() {
+        // 手动注册数据源
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:postgresql://pg-sit.deepq.tech:5432/deliver_poc?currentSchema=mj_items");
+        hikariConfig.setUsername("deliver_poc");
+        hikariConfig.setPassword("Deepq@deliver_poc2022");
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
         hikariConfig.setConnectionTestQuery("SELECT 1");
         hikariConfig.setMaximumPoolSize(10);
         hikariConfig.setMinimumIdle(2);
